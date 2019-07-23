0396c1d98ca0d07fa4b5c9ee9e8ce3142126bbea
caiyin <c763416559@gmail.com>
2019-07-23 14:17:39 +0800

    feat: 更新 README

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import MyComponents from '@lib/vue-cli-project-demo.umd.min.js';
+import MyComponents from '@lib/vue-cli-project-demo.umd.min';
+// import MyComponents from './entry';
 
 Vue.config.productionTip = false;
 
